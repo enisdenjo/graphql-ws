cc6bc816d370e54bb59bdb7487b9c80b120dd78f
enisdenjo <badurinadenis@gmail.com>
2020-08-18 16:11:01 +0200

    ci: dont commit lib assets

--- lib/protocol.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- *
- * protocol
- *
- */
-/** The WebSocket sub-protocol used for the [GraphQL over WebSocket Protocol](/PROTOCOL.md). */
-export declare const GRAPHQL_TRANSPORT_WS_PROTOCOL = "graphql-transport-ws";
