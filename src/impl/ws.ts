684c2ddd382f36b3230e079748cc26c0f304c3ae
enisdenjo <badurinadenis@gmail.com>
2020-11-12 00:54:05 +0100

    feat: ping-pong when using with ws

--- src/impl/ws.ts
+++ src/impl/ws.ts
@@ -1,15 +1,43 @@
 import { Server as WebSocketServer } from 'ws';
 import { Server } from '../server';
 
+const keepAlive = 12 * 1000; // 12 seconds
+
 /**
  * Use the server on a [ws](https://github.com/websockets/ws) WebSocket server.
  */
 export function useServer(server: Server): WebSocketServer {
   const wsServer = new WebSocketServer();
   wsServer.on('connection', (socket) => {
+    // keep alive through ping-pong messages
+    let pongWait: NodeJS.Timeout | null = null;
+    const pingInterval = setInterval(() => {
+      // ping pong on open sockets only
+      if (socket.readyState === WebSocket.OPEN) {
+        // terminate the connection after pong wait has passed because the client is idle
+        pongWait = setTimeout(() => {
+          socket.terminate();
+        }, keepAlive);
+
+        // listen for client's pong and stop socket termination
+        socket.once('pong', () => {
+          if (pongWait) {
+            clearTimeout(pongWait);
+            pongWait = null;
+          }
+        });
+
+        socket.ping();
+      }
+    }, keepAlive);
+
     // server calls `waitForClose` later, it will resolve immediately
     const waitForClose = new Promise<void>(
-      (resolve) => (socket.onclose = () => resolve()),
+      (resolve) =>
+        (socket.onclose = () => {
+          clearInterval(pingInterval);
+          resolve();
+        }),
     );
 
     server.opened({
