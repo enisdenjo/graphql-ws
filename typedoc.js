0f4efbabd2923b5362580beeb55b092d6a67d18e
enisdenjo <badurinadenis@gmail.com>
2021-08-21 11:17:28 +0200

    docs: exclude private [skip ci]

--- typedoc.js
+++ typedoc.js
@@ -3,6 +3,7 @@ module.exports = {
   readme: 'none',
   plugin: 'typedoc-plugin-markdown',
   excludeExternals: true,
+  excludePrivate: true,
   disableSources: true,
   categorizeByGroup: false, // removes redundant category names in matching modules
   exclude: ['**/index.ts', '**/utils.ts', '**/__tests__/**/*'],
