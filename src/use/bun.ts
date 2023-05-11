648b5f45d6eec479e1ab797dedb0203857b2a831
enisdenjo <badurinadenis@gmail.com>
2023-05-12 00:39:44 +0200

    docs(use/bun): subprotocol and example

--- src/use/bun.ts
+++ src/use/bun.ts
@@ -28,8 +28,37 @@ export interface Extra {
  * Use the server with [Bun](https://bun.sh/).
  * This is a basic starter, feel free to copy the code over and adjust it to your needs
  *
- * The keep-alive logic _seems_ to be handled by Bun seeing that
+ * The WebSocket subprotocol is not available on the established socket and therefore
+ * needs to be checked during the request handling.
+ *
+ * Additionally, the keep-alive logic _seems_ to be handled by Bun seeing that
  * they default [`sendPingsAutomatically` to `true`](https://github.com/oven-sh/bun/blob/6a163cf933542506354dc836bd92693bcae5939b/src/deps/uws.zig#L893).
+ *
+ * ```ts
+ * import { makeHandler, handleProtocols } from 'graphql-ws/lib/use/lib/bun';
+ * import { schema } from "./my-schema";
+ *
+ * Bun.serve({
+ *   fetch(req, server) {
+ *     const [path, _search] = req.url.split('?');
+ *     if (
+ *       path.endsWith('/graphql') &&
+ *       handleProtocols(req.headers.get('sec-websocket-protocol') || '')
+ *     ) {
+ *       if (server.upgrade(req)) {
+ *         return new Response();
+ *       } else {
+ *         return new Response('Internal Server Error', { status: 500 });
+ *       }
+ *     }
+ *     return new Response('Upgrade Required', { status: 426 });
+ *   },
+ *   websocket: makeHandler({ schema }),
+ *   port: 4000,
+ * });
+ *
+ * console.log('Listening to port 4000');
+ * ```
  *
  * @category Server/bun
  */
