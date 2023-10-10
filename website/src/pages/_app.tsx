5095c73c03decaa9de3923e9d41a377f11330407
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-11 00:52:27 +0200

    prettier

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@theguild/components/style.css';
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- website/src/pages/_meta.ts
+++ website/src/pages/_meta.ts
@@ -1,13 +1,13 @@
 export default {
-  'index': {
-    'title': 'Index',
-    'type': 'page',
-    'display': 'hidden',
-    'theme': {
-      'layout': 'raw'
-    }
+  index: {
+    title: 'Index',
+    type: 'page',
+    display: 'hidden',
+    theme: {
+      layout: 'raw',
+    },
   },
   'get-started': 'Get Started',
-  'recipes': 'Recipes',
-  'docs': 'Documentation'
-}
+  recipes: 'Recipes',
+  docs: 'Documentation',
+};
