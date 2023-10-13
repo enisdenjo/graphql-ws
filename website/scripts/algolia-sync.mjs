81b5598370a60eef7572799f11a47a4d2bea5c69
enisdenjo <denis@denelop.com>
2023-10-13 16:54:31 +0200

    docs: try alpha algolia sync

--- website/scripts/algolia-sync.mjs
+++ website/scripts/algolia-sync.mjs
@@ -7,5 +7,6 @@ indexToAlgolia({
   source: 'WS',
   domain: process.env.SITE_URL,
   lockfilePath: 'algolia-lockfile.json',
+  sitemapXmlPath: 'public/sitemap.xml',
   dryMode: process.env.ALGOLIA_DRY_RUN === 'true',
 });
