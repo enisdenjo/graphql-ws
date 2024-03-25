8f905bdc0eede6d175cc2077603329fba28b0d4a
Gil Gardosh <gilgardosh@gmail.com>
2024-03-25 13:46:15 +0200

    docs: bump @theguild/components and migrate (#549)
    Co-authored-by: enisdenjo <denis@denelop.com>

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,2 +1,5 @@
+// @ts-expect-error the guild's next config doesnt have types
 import { withGuildDocs } from '@theguild/components/next.config';
-export default withGuildDocs();
+export default withGuildDocs({
+  output: 'export',
+});
--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,6 +1,8 @@
-import { defineConfig } from '@theguild/components';
+import { defineConfig, PRODUCTS } from '@theguild/components';
 
 export default defineConfig({
-  siteName: 'WS',
+  websiteName: PRODUCTS.WS.name,
+  description: PRODUCTS.WS.title,
   docsRepositoryBase: 'https://github.com/enisdenjo/graphql-ws',
+  logo: PRODUCTS.WS.logo({ className: 'w-9' }),
 });
