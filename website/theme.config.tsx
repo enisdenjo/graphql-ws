fb597e0f8c1f1803d5c3ed084dcf609c4af8c1b5
enisdenjo <denis@denelop.com>
2024-03-25 12:35:37 +0100

    adjust size of logo

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -4,5 +4,5 @@ export default defineConfig({
   websiteName: PRODUCTS.WS.name,
   description: PRODUCTS.WS.title,
   docsRepositoryBase: 'https://github.com/enisdenjo/graphql-ws',
-  logo: PRODUCTS.WS.logo,
+  logo: PRODUCTS.WS.logo({ className: 'w-8' }),
 });
