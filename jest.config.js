8ee7de3785eae2bf4bd57220e3f4959b640fe5ef
Matthew Stern <16885220+sternma@users.noreply.github.com>
2024-11-07 23:18:57 -0500

    fix tests to accomodate new ws browser.js export

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,8 @@ const opts = {
   testRunner: 'jest-jasmine2', // until https://github.com/facebook/jest/issues/11698 and hopefully https://github.com/facebook/jest/issues/10529
   moduleFileExtensions: ['ts', 'js'],
   testPathIgnorePatterns: ['/node_modules/', '/fixtures/', '/utils/', 'd.ts$'],
+  moduleNameMapper: {
+    "^ws$": "<rootDir>/node_modules/ws/index.js"
+  }
 };
 module.exports = opts;
